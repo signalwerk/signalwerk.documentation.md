c1505c147cc65a19daa141b419cb62e102af1588
signalwerk <sh@signalwerk.ch>
2023-12-25 23:28:01 +0100

    MOD: update template

--- template/src/config.jsx
+++ template/src/config.jsx
@@ -1,36 +1,28 @@
 import React from "react";
 
-const libraryStyleSheets = [
-  "https://fonts.signalwerk.ch/css/latest/family=Work+Sans:ital,wght@0,100..900;1,100..900.css",
-];
-
-function root(data, { Helmet, processor }) {
-  return (
-    <>
-      <Helmet>
-        {libraryStyleSheets.map((url) => (
-          <link href={url} rel="stylesheet" />
-        ))}
-        <link rel="stylesheet" href="/style.css" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Helmet>
-      {data?.children?.map((item, index) => (
-        <>{processor.run(item)}</>
-      ))}
-    </>
-  );
-}
+// function root(data, { Helmet, processor }) {
+//   if (!data) return null;
+//   return (
+//     <>
+//       <Helmet>
+//         <link rel="stylesheet" href="/style.css" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//       </Helmet>
+//       <>{data.children && processor.run(data.children)}</>
+//     </>
+//   );
+// }
 
 const config = {
-  types: {
-    ":root": root,
-  },
-  admin: {
-    init: ({ CMS }) => {
-      libraryStyleSheets.forEach((url) => {
-        CMS.registerPreviewStyle(url);
-      });
-    },
-  },
+  // types: {
+  //   ":root": root,
+  // },
+  // for the admin interface
+  // admin: {
+  //   // for init the admin interface
+  //   init: ({ CMS }) => {
+  //     CMS.registerPreviewStyle(url);
+  //   },
+  // },
 };
 export default config;
