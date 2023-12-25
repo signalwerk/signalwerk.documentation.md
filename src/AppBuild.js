97b21f72a83a905245e1aa4e242e88352900b90f
signalwerk <sh@signalwerk.ch>
2023-12-25 02:47:20 +0100

    ADD: prettier

--- src/AppBuild.js
+++ src/AppBuild.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 import { renderToString } from "react-dom/server";
 import { Helmet } from "react-helmet";
 import { typeProcessor } from "./components";
-// // import prettier from "prettier";
+import prettier from "prettier";
 import { fixPage } from "./utils/fixPage.js";
 import config from "../../../src/config.jsx";
 import { getFilesFromDir } from "./getFilesFromDir";
@@ -45,14 +45,15 @@ pagePath.forEach((originalPath) => {
     </body>
   </html>`;
 
-  // const formattedHtml = await prettier.format(htmlString, { parser: "html" });
-  // fs.writeFileSync(outputHtml, formattedHtml);
-
   fs.mkdirSync(path.join(outputRoot, slug), { recursive: true }, (err) => {
     if (err) throw err;
   });
 
-  fs.writeFileSync(path.join(outputRoot, slug, outputHtml), htmlString);
+  const formattedHtml = prettier
+    .format(htmlString, { parser: "html" })
+    .then((formattedHtml) => {
+      fs.writeFileSync(path.join(outputRoot, slug, outputHtml), formattedHtml);
 
-  console.log("!!!!!!! finish index");
+      console.log(`build  ${slug}`);
+    });
 });
