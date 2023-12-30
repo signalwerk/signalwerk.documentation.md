1c7c663487f40a6965f30a215885b3a280a3678d
signalwerk <sh@signalwerk.ch>
2023-12-30 03:16:08 +0100

    FIX: build

--- src/AppBuild.js
+++ src/AppBuild.js
@@ -24,6 +24,17 @@ const contentRoot = path.join(mainRoot, "./content");
 
 export const pagePath = getFilesFromDir(contentRoot, ".json");
 
+const pathCache = {}; // map from slug to original path
+const pageData = {}; // data with id = path
+
+pagePath.forEach((originalPath) => {
+  const content = fs.readFileSync(originalPath, "utf-8");
+  const data = JSON.parse(content);
+  pageData[originalPath] = data;
+  let slug = data.path;
+  pathCache[slug] = originalPath;
+});
+
 const stylesheet = fs.readFileSync(
   path.join(outputRoot, stylesheetPath),
   "utf8"
@@ -31,15 +42,17 @@ const stylesheet = fs.readFileSync(
 const hash = crypto.createHash("md5").update(stylesheet).digest("hex"); // create hash from stylesheet
 
 pagePath.forEach((originalPath) => {
-  const dataContent = fs.readFileSync(originalPath, "utf8");
+  const data = pageData[originalPath];
+  const slug = data.path;
 
-  const data = JSON.parse(dataContent);
-  let slug = data.path;
+  // const dataContent = fs.readFileSync(originalPath, "utf8");
 
-  const page = fixPage(data);
+  // const data = JSON.parse(dataContent);
 
-  const content = renderToString(typeProcessor(page, { config, settings }))
-    // Remove `html injected
+  const page = fixPage(data, { settings, data: pageData, pathCache });
+
+  const content = renderToString(typeProcessor(page, { config, settings }));
+  // Remove `html injected
 
   const helmet = Helmet.renderStatic(); // Extract head data
 
