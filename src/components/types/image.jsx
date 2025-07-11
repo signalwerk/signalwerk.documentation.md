2bc43c29a6ea6b90082786503b54c40494e495a4
signalwerk <sh@signalwerk.ch>
2025-07-11 17:43:49 +0300

    MOD: adjust formatting

--- src/components/types/image.jsx
+++ src/components/types/image.jsx
@@ -1,17 +1,11 @@
 import { text } from "./text.jsx";
 
-
 export function image(node, { alt } = {}) {
   if (!node) return null;
   if (!node.path) return null;
   return (
     <div className={`node-image ${node.class || ""}`}>
-      {
-        <img
-          src={node.path}
-          alt={node.alt || node.caption || alt || "picture"}
-        />
-      }
+      <img src={node.path} alt={node.alt || node.caption || alt || "picture"} />
       {node.caption && <div className="node-image__caption">{text(node)}</div>}
     </div>
   );
